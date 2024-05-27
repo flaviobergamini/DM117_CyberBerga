5ae54ce8029e092fc4fe56fee5519e8325d92a4b
unknown <flaviohenrique@gec.inatel.br>
2024-05-27 13:44:35 -0300

    Feat: Adding articulated character assets, replacing the initial character, creating movement animation

--- Assets/Controllers/PlayerController.cs
+++ Assets/Controllers/PlayerController.cs
@@ -8,23 +8,32 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float angleSpeed;
 
-    private float moveX, MoveZ;
+    private float moveX, moveZ;
+
     Rigidbody rigidBody;
+
+    Animator animator;
 
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
     {
         moveX = Input.GetAxis("Horizontal");
-        MoveZ = Input.GetAxis("Vertical");
+        moveZ = Input.GetAxis("Vertical");
+
+        if (moveZ != 0)
+            animator.SetBool("isWalking", true);
+        else
+            animator.SetBool("isWalking", false);
     }
 
     private void FixedUpdate()
     {
-        rigidBody.velocity = transform.forward * MoveZ * speed;
+        rigidBody.velocity = transform.forward * moveZ * speed;
 
         rigidBody.MoveRotation(rigidBody.rotation * Quaternion.Euler(0f, angleSpeed * moveX, 0f));
     }
