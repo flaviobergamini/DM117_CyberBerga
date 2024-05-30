cde27134c4c62ff7f4ad3bc3c853686f78ba308b
unknown <flaviohenrique@gec.inatel.br>
2024-05-30 19:09:50 -0300

    Feat: Creating NPC mechanics for both scenes

---
+++ Assets/Controllers/EnemySpaceshipEnemyController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class EnemySpaceshipEnemyController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    GameObject player;
+
+    void Start()
+    {
+        player  = GameObject.Find("SpaceshipPlayer");
+    }
+
+
+    void Update()
+    {
+        transform.LookAt(player.transform.position);
+
+        if(transform.position == player.transform.position)
+        {
+            Destroy(this.gameObject);
+            
+        }
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/RobotSphere/Assets/Scripts/RobotFreeAnim.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public class RobotFreeAnim : MonoBehaviour
+{
+
+    Vector3 rot = Vector3.zero;
+    float rotSpeed = 40f;
+    Animator anim;
+
+    // Use this for initialization
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        gameObject.transform.eulerAngles = rot;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var player = GameObject.Find("PlayerHuman");
+
+        transform.LookAt(player.transform.position);
+
+        if (transform.position == player.transform.position)
+        {
+            Destroy(this.gameObject);
+
+        }
+
+        transform.Translate(Vector3.forward * 0.5f * Time.deltaTime);
+
+        CheckKey();
+
+        gameObject.transform.eulerAngles = rot;
+    }
+
+    void CheckKey()
+    {
+        // Walk
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("Walk_Anim", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetBool("Walk_Anim", false);
+        }
+
+        // Rotate Left
+        if (Input.GetKey(KeyCode.A))
+        {
+            rot[1] -= rotSpeed * Time.fixedDeltaTime;
+        }
+
+        // Rotate Right
+        if (Input.GetKey(KeyCode.D))
+        {
+            rot[1] += rotSpeed * Time.fixedDeltaTime;
+        }
+
+        // Roll
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (anim.GetBool("Roll_Anim"))
+            {
+                anim.SetBool("Roll_Anim", false);
+            }
+            else
+            {
+                anim.SetBool("Roll_Anim", true);
+            }
+        }
+
+        // Close
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            if (!anim.GetBool("Open_Anim"))
+            {
+                anim.SetBool("Open_Anim", true);
+            }
+            else
+            {
+                anim.SetBool("Open_Anim", false);
+            }
+        }
+    }
+
+}
